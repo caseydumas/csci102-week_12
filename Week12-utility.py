9231550833373f79c1bd11d5910918ec7adfdb9c
Casey Dumas <kdumas@mymail.mines.com>
2019-11-21 11:31:17 -0700

    updated Week12-utility

--- Week12-utility.py
+++ Week12-utility.py
@@ -13,6 +13,13 @@ def UpdateString(string_one, string_two, index):
     string_one_list[index] = string_two
     joined_string = ''.join(string_one_list)
     print(joined_string)
+
+def FindWordCount(input_list, input_string):
+    count = 0
+    for i in input_list:
+        if input_list[i] == input_string:
+            count += 1
+            
 PrintOutput('hi')
 LoadFile("C:/Users/casey/csci102-week_12/README.md")
 UpdateString('hello there', 'ye', 4)

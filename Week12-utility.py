9931681a74a7eb8a70befeffaae67a0924ef20c4
Casey Dumas <kdumas@mymail.mines.com>
2019-11-21 11:30:11 -0700

    updated Week12-utility

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,7 +1,18 @@
 def PrintOutput(string):
     output = print('OUTPUT '+str(string))
+
+    
 def LoadFile(file):
     f = open(file, 'r')
-    lines = f.readlines()
-PrintOutput()
-LoadFile()
+    lines = f.read().splitlines()
+    print('Output'+str(lines))
+
+
+def UpdateString(string_one, string_two, index):
+    string_one_list = list(string_one)
+    string_one_list[index] = string_two
+    joined_string = ''.join(string_one_list)
+    print(joined_string)
+PrintOutput('hi')
+LoadFile("C:/Users/casey/csci102-week_12/README.md")
+UpdateString('hello there', 'ye', 4)
